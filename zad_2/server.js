b6637ab2de0319a43c3bc5680a333a6040dc9339
mjalowiczor <m.jalowiczor@hotmail.com>
2020-02-25 20:42:16 +0100

    Add navbar with change sites funcionality

--- zad_2/server.js
+++ zad_2/server.js
@@ -1,35 +1,39 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("express-handlebars");
 
 const app = express();
+app.engine("hbs", hbs({ extname: "hbs", defaultLayout: "main" }));
+app.set("view engine", "hbs");
 
-app.use((req, res, next) => {
-  res.show = name => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
-
-app.use("/public", express.static(path.join(__dirname, "public")));
-
-app.use("/user/", (req, res, next) => {
-  res.show("forbidden.html");
-});
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.get("/", (req, res) => {
-  res.show("home.html");
+  res.render("home");
 });
 
-app.get("/home", (req, res) => {
-  res.show("home.html");
-});
+// app.get("/hello/:name", (req, res) => {
+//   res.render("hello", { layout: false, name: req.params.name });
+// });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/info", (req, res) => {
+  res.render("info");
+});
+
+app.get("/history", (req, res) => {
+  res.render("history");
 });
 
 app.use((req, res) => {
-  res.show("error404.html");
+  res.status(404).send("404 not found...");
 });
 
 app.listen(8000, () => {
