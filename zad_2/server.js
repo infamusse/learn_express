462440b539366bad4f2b416d346ed7f510d12083
mjalowiczor <m.jalowiczor@hotmail.com>
2020-02-26 22:48:58 +0100

    resolving problem with dont show image on 404 page and redirect to forbidden if user dont logged

--- zad_2/server.js
+++ zad_2/server.js
@@ -6,15 +6,15 @@ const app = express();
 app.engine("hbs", hbs({ extname: "hbs", defaultLayout: "main" }));
 app.set("view engine", "hbs");
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use("/public", express.static(path.join(__dirname + "/public")));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// app.get("/hello/:name", (req, res) => {
-//   res.render("hello", { layout: false, name: req.params.name });
-// });
+app.use("/user/", (req, res, next) => {
+  res.render("forbidden");
+});
 
 app.get("/about", (req, res) => {
   res.render("about");
@@ -33,7 +33,7 @@ app.get("/history", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).send("404 not found...");
+  res.render("error404");
 });
 
 app.listen(8000, () => {
